97eff8f72d90df67511b11613ef298e58686e4b1
Tzero Ocne <tzeroocne.interface@gmail.com>
2025-05-01 10:41:23 +0700

    feat(build): add build options

--- zig/build.zig
+++ zig/build.zig
@@ -1,16 +1,20 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const exe = b.addExecutable(.{
-        .name = "hello",
-        .root_source_file = b.path("src/hello.zig"),
-        .target = b.graph.host,
-    });
+    const options = b.option(u16, "app", "App to build") orelse 0;
 
-    b.installArtifact(exe);
+    if (options == 1) {
+        const exe = b.addExecutable(.{
+            .name = "hello",
+            .root_source_file = b.path("src/hello.zig"),
+            .target = b.graph.host,
+        });
 
-    const run_exe = b.addRunArtifact(exe);
+        b.installArtifact(exe);
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_exe.step);
+        const run_exe = b.addRunArtifact(exe);
+
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_exe.step);
+    }
 }
