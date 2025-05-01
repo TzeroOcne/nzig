412f0975ec29bb143fc8b62fa71d85194995691e
Tzero Ocne <tzeroocne.interface@gmail.com>
2025-05-01 11:13:59 +0700

    feat(build): add build static and dynamic library options

--- zig/build.zig
+++ zig/build.zig
@@ -16,5 +16,40 @@ pub fn build(b: *std.Build) void {
 
         const run_step = b.step("run", "Run the app");
         run_step.dependOn(&run_exe.step);
+    } else if (options == 2) {
+        const target = b.standardTargetOptions(.{});
+        const optimize = b.standardOptimizeOption(.{});
+
+        if (b.option(bool, "enable-demo", "install the demo too") orelse false) {
+            const libfizzbuzz = b.addStaticLibrary(.{
+                .name = "fizzbuzz",
+                .root_source_file = b.path("src/fizzbuzz.zig"),
+                .target = target,
+                .optimize = optimize,
+            });
+
+            b.installArtifact(libfizzbuzz);
+
+            const exe = b.addExecutable(.{
+                .name = "demo",
+                .root_source_file = b.path("src/demo.zig"),
+                .target = target,
+                .optimize = optimize,
+            });
+
+            exe.linkLibrary(libfizzbuzz);
+
+            b.installArtifact(exe);
+        } else if (b.option(bool, "build-shared", "Build a shared library") orelse false) {
+            const libfizzbuzz = b.addSharedLibrary(.{
+                .name = "fizzbuzz",
+                .root_source_file = b.path("src/fizzbuzz.zig"),
+                .target = target,
+                .optimize = optimize,
+                .version = .{ .major = 0, .minor = 0, .patch = 1 },
+            });
+
+            b.installArtifact(libfizzbuzz);
+        }
     }
 }
